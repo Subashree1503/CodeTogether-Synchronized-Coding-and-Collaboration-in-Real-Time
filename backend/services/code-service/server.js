d358e41f9e99db7f588b7b82508b991db07ab0cb
InduVarshini <induvarshinij2000@gmail.com>
2024-11-27 15:56:48 -0700

    Added code sharing service

---
+++ backend/services/code-service/server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, { cors: { origin: '*' } });
+
+const rooms = {};
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  socket.on('JOIN_ROOM', ({ roomId, username }) => {
+    socket.join(roomId);
+    if (!rooms[roomId]) rooms[roomId] = [];
+    rooms[roomId].push(username);
+    io.to(roomId).emit('USER_JOINED', rooms[roomId]);
+  });
+
+  socket.on('CODE_CHANGED', ({ roomId, code }) => {
+    io.to(roomId).emit('CODE_CHANGED', code);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  });
+});
+
+server.listen(6000, () => console.log('Code Collaboration Service running on port 6000'));
